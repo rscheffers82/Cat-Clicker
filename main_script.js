85ab55b2182f5c3b150af18bf9c61b20d9b16d51
Roy Scheffers <r.scheffers@gmail.com>
2016-06-27 19:54:48 -0400

    Add full functionality

--- main_script.js
+++ main_script.js
@@ -6,41 +6,71 @@ var CatCollection = function(){
 	var clicks = 0;
 	var cats = [{
 		name : 'Maine Coon',
-		url: 'http://www.pethealthnetwork.com/sites/default/files/maine-coon-cat-484757920.jpg'
+		url: 'http://www.pethealthnetwork.com/sites/default/files/maine-coon-cat-484757920.jpg',
+		clicks: 0
 	},
 	{
 		name: 'Zoki smart kitty',
-		url: 'https://image.freepik.com/free-photo/small-cat-cat-picture-material_38-4248.jpg'
+		url: 'https://image.freepik.com/free-photo/small-cat-cat-picture-material_38-4248.jpg',
+		clicks: 0
+	},
+	{
+		name: 'Grumpy Cat is not amused',
+		url: 'http://cdn.images.express.co.uk/img/dynamic/80/590x/Grumpy-Cat-on-bed-544409.jpg',
+		clicks: 0
+	},
+	{
+		name: 'Black cat brings you luck',
+		url: 'http://www.catster.com/wp-content/uploads/2015/06/600px-black-cat-in-meadow.jpg',
+		clicks: 0
+	},
+	{
+		name: 'Love the snow cat',
+		url: 'http://taildom.com/blog/wp-content/uploads/2012/02/cat-snow.jpg',
+		clicks: 0
 	}];
 
-	this.display = function(){
-		var html = '';
-		for (x = 0; x < cats.length; x++ ) {
-		html += '<div class="catContainer">';
-				html += '<div class="catName">' + cats[x].name + '</div>';
-				html += '<img class="catImage" src="' + cats[x].url + '"alt="' + cats[x].name + '">';
-		html += '</div>';
+	this.displayList = function(){
+		/*
+		image
+		place in the array
+		*/
+		html = '';
+		for ( x = 0; x < cats.length; x++ ) {
+			html += '<img class="list-item" id="' + x + '" src="' + cats[x].url + '">';
 		}
-		$('#cats').append(html);
+		$('#catList').html(html);
+	}
+	this.displayCat = function(c){
+		var html = '';
+			html += '<div class="catName">' + cats[c].name + '</div>';
+			html += '<img class="catImage" id="' + c + '" src="' + cats[c].url + '" alt="' + cats[c].name + '">';
+			html += '<div class="catClicks">' + cats[c].clicks + '</div>';
+		$('#catContainer').html(html);
+
 	}
 
-	this.clicked = function(){
+	this.catClicked = function(catId){
 		console.log('within clicked');
-		clicks++;
-		console.log(clicks);
-		$('#clicks').text(clicks);
+		cats[catId].clicks++;
+		console.log(cats[catId].clicks);
+		$('.catClicks').text(cats[catId].clicks);
 	}
-
 }
 
 var catCol = new CatCollection();
-catCol.display();
+catCol.displayList();
 
+// used for displaying the clicked cat in the list 
+$('.list-item').click(function(e){
+	//console.log(e);
+	var chosenOne = e.target.id;
+	catCol.displayCat(chosenOne);
+});
 
-$('.catImage').click(function(){
-	catCol.clicked();
-	/*var num = $('#clicks').text();
-	num = Number(num);
-	num++;
-	$('#clicks').text(num);*/
+// used for handling the clicked cat in the main display
+$('#catContainer').on('click', function(e){
+	console.log('display cat clicked');
+	var catId = e.target.id;
+	catCol.catClicked(catId);
 });
